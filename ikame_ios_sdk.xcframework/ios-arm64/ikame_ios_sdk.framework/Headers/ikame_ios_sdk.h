46668c61857ab67afbace0454953cfaea0bd0686
XuanNguyen12 <117880646+XuanNguyen12@users.noreply.github.com>
2024-12-17 10:17:41 +0700

    Add files via upload

---
+++ ikame_ios_sdk.xcframework/ios-arm64/ikame_ios_sdk.framework/Headers/ikame_ios_sdk.h
@@ -0,0 +1,18 @@
+//
+//  ikame_ios_sdk.h
+//  ikame_ios_sdk
+//
+//  Created by nguyen xuan on 20/11/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ikame_ios_sdk.
+FOUNDATION_EXPORT double ikame_ios_sdkVersionNumber;
+
+//! Project version string for ikame_ios_sdk.
+FOUNDATION_EXPORT const unsigned char ikame_ios_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ikame_ios_sdk/PublicHeader.h>
+
+
